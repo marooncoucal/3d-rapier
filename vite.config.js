ea288d304266f1676619718947e1e4c59da77268
marooncoucal <marooncoucal@gmail.com>
2025-11-04 11:39:15 +0300

    i think it worked? rapier3d-compat 0.15.0

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import wasm from 'vite-plugin-wasm'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), wasm()],
+  optimizeDeps: {
+    // exclude Rapier runtime pre-bundling so wasm is loaded at runtime intact
+    exclude: ["@dimforge/rapier3d-compat", "@dimforge/rapier3d"]
+  },
+  assetsInclude: ["**/*.wasm"]
 })
